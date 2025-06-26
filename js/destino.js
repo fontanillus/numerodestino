eea3f924f050e5c78226298a93f82683c40a5897
fontanillus <arcamapa@gmail.com>
2025-06-26 12:29:26 +0200

    Add files via upload

---
+++ js/destino.js
@@ -0,0 +1,13 @@
+const significados = [
+                { numero: 1, significado: 'Liderazgo, independencia, iniciativa.' },
+                { numero: 2, significado: 'Diplomacia, cooperación, sensibilidad.' },
+                { numero: 3, significado: 'Creatividad, expresión, alegría.' },
+                { numero: 4, significado: 'Estabilidad, trabajo duro, organización.' },
+                { numero: 5, significado: 'Aventura, libertad, versatilidad.' },
+                { numero: 6, significado: 'Armonía, responsabilidad, servicio.' },
+                { numero: 7, significado: 'Intuición, análisis, espiritualidad.' },
+                { numero: 8, significado: 'Ambición, éxito material, poder.' },
+                { numero: 9, significado: 'Humanitarismo, compasión, sabiduría.' },
+                { numero: 11, significado: 'Inspiración, idealismo, creatividad y expresión. Sanadores y motivadores.' },
+                { numero: 22, significado: 'Visionario/a para transformar el mundo. Construcción, manifestación, pragmatismo.' }
+            ];
